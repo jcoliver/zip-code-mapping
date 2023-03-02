2dd7159339aad053d0f026a439310c170f22db0f
Jeff Oliver <jcoliver@email.arizona.edu>
2023-03-02 16:28:43 -0700

    Update with larger dataset

--- zip-mapping.R
+++ zip-mapping.R
@@ -12,28 +12,30 @@ zip_shapes <- st_read("data/AZ_ZipCodes/AZ_ZipCodes.shp")
 
 # Lots of census data in there, just want zip codes
 zips <- zip_shapes[, c("ZIP", "geometry")]
-plot(zips)
+# plot(zips)
 
 # Reading in just one file
-high <- read.csv(file = "data/high.csv")
+rural <- read.csv(file = "data/rural_classification.csv")
 
 # Need to get Zip codes back to five-digit
-high <- high %>%
+rural <- rural %>%
   mutate(zip_char = as.character(three_digit_zip)) %>%
   mutate(zip_five = case_when(nchar(zip_char) == 2 ~ paste0("850", zip_char),
                             nchar(zip_char) == 3 ~ paste0("85", zip_char),
                             TRUE ~ NA_character_)) %>%
-  select(zip_five, percentage)
+  filter(zip_tag == "Positive") %>%
+  select(zip_five, percentage, three_digit_zip)
 
 # Join the percentage data with the shapefile data
 combined <- zips %>%
-  left_join(high, by = c("ZIP" = "zip_five"))
+  left_join(rural, by = c("ZIP" = "zip_five"))
 
 # Now plot
-high_plot <- ggplot(data = combined,
+rural_plot <- ggplot(data = combined,
                     mapping = aes(fill = percentage, color = percentage)) +
   geom_sf() +
   scale_fill_distiller(palette = "BrBG") +
   scale_color_distiller(palette = "BrBG") +
   theme_bw()
-ggsave(filename = "example-high.png", plot = high_plot)
+rural_plot
+ggsave(filename = "example-rural.png", plot = rural_plot)
